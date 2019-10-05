7370723d1c450a17006cdce1ce657107afc3137f
sethherr <seth.william.herr@gmail.com>
2019-10-05 14:23:48 -0700

    apply fix to v1 too

--- app/controllers/api/v1/incident_finder.rb
+++ app/controllers/api/v1/incident_finder.rb
@@ -2,14 +2,13 @@ module API
   module V1
     module IncidentFinder
       extend ActiveSupport::Concern
-      included do 
-
-        helpers do 
+      included do
+        helpers do
           def find_incidents
             incidents = Incident.all
             if params[:updated_since].present?
-              if params[:occurred_since] == 'yesterday'
-                date = (Time.now - 1.days) 
+              if params[:occurred_since] == "yesterday"
+                date = (Time.now - 1.days)
               else
                 date = Time.at(params[:updated_since].to_i).utc.to_datetime
               end
@@ -29,14 +28,20 @@ module API
             end
             if params[:incident_types].present?
               incident_types = params[:incident_types]
-              incident_types = incident_types.split(',') unless incident_types.kind_of?(Array)
-              ids = incident_types.map{ |k| IncidentType.fuzzy_find_id(k) }
+              incident_types = incident_types.split(",") unless incident_types.kind_of?(Array)
+              ids = incident_types.map { |k| IncidentType.fuzzy_find_id(k) }
               incidents = incidents.where("incident_type_id IN (?)", ids)
             end
             if params[:location].present?
               width = params[:location_width].present? ? params[:location_width] : 50
               box = Geocoder::Calculations.bounding_box(params[:location], width)
-              incidents = incidents.with_location.within_bounding_box(box)
+              # If the proximity is an invalid location, some of the box coordinates will be NotANumber
+              # just return no incidents
+              if box.any? { |l| l.nan? }
+                incidents = incidents.where(id: nil) # hack to make it no locations
+              else
+                incidents = incidents.with_location.within_bounding_box(box)
+              end
             end
             if params[:query].present?
               incidents = incidents.search_text(params[:query])
@@ -44,7 +49,6 @@ module API
             incidents
           end
         end
-
       end
     end
   end
