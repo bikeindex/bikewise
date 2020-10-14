407ba2874c3e771b4b549518f87816665b3fb8b9
sethherr <seth.william.herr@gmail.com>
2020-10-14 16:08:19 -0700

    better fog

--- config/initializers/carrierwave_fog.rb
+++ config/initializers/carrierwave_fog.rb
@@ -2,13 +2,17 @@ require 'yaml'
 
 CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/"
-  config.fog_provider "fog/aws"
-  config.fog_credentials = {
-    provider:               'AWS',       # required
-    aws_access_key_id:      ENV['S3_ACCESS_KEY'],
-    aws_secret_access_key:  ENV['S3_SECRET_KEY'],
-    region:                 'us-west-2'  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = ENV['S3_BUCKET']
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} 
+  if Rails.env.production?
+    config.fog_provider "fog/aws"
+    config.fog_credentials = {
+      provider:               'AWS',       # required
+      aws_access_key_id:      ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key:  ENV['S3_SECRET_KEY'],
+      region:                 'us-west-2'  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} 
+  else
+    config.storage :file
+  end
 end
