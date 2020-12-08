4a5a41acb7b2365dbd707807c9c20a8ad6170f71
sethherr <seth.william.herr@gmail.com>
2020-12-08 10:01:54 -0800

    carrierwave config

--- config/initializers/carrierwave_fog.rb
+++ config/initializers/carrierwave_fog.rb
@@ -3,7 +3,7 @@ require 'yaml'
 CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/"
   if Rails.env.production?
-    config.fog_provider "fog/aws"
+    config.storage = :fog
     config.fog_credentials = {
       provider:               'AWS',       # required
       aws_access_key_id:      ENV['S3_ACCESS_KEY'],
